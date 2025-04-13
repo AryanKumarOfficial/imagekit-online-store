22febc1ff96f1a081208a00f72c35cb008232ca2
Aryan Kumar <aryanak9163@gmail.com>
2025-04-13 23:58:17 +0530

    imagekit-auth: add imagekit auth route

---
+++ src/app/api/imagekit-auth/route.ts
@@ -0,0 +1,23 @@
+import ImageKit from "imagekit";
+import { NextResponse } from "next/server";
+
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT!,
+});
+
+export async function GET() {
+  try {
+    const authenticationParameters = imagekit.getAuthenticationParameters();
+    return NextResponse.json(authenticationParameters, {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error generating authentication signature:", error);
+    return NextResponse.json(
+      { error: "Failed to generate authentication signature" },
+      { status: 500 }
+    );
+  }
+}
