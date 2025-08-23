eb637f96ad4571681b81208bcbdb4fd177308dc1
Aryan Kumar <aryanak9163@gmail.com>
2025-08-23 22:14:03 +0530

    add methods to change and forget password

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,19 +1,29 @@
-import mongoose, {model, models, Schema} from "mongoose";
+import mongoose, {Document, model, models, Schema} from "mongoose";
 
 import bcrypt from "bcryptjs";
-import {DateTime} from "next-auth/providers/kakao";
 
 export interface IUser {
     _id?: mongoose.Types.ObjectId;
     email: string;
     password: string;
     role: "user" | "admin";
-    isVerified: DateTime | null;
+    isVerified: Date | null;
     createdAt?: Date;
     updatedAt?: Date;
 }
 
-const userSchema = new Schema<IUser>(
+interface IUserSchema extends mongoose.Model<IUser> {
+    changePassword: (email: string, password: string, newPassword: string) => Promise<void>;
+    forgetPassword: (email: string, password: string) => Promise<void>;
+}
+
+interface IUserMethods {
+    comparePassword: (candidatePassword: string) => Promise<boolean>;
+}
+
+type IUserDocument = IUser & IUserMethods & Document;
+
+const userSchema = new Schema<IUserDocument, IUserSchema, IUserMethods>(
     {
         email: {
             type: String,
@@ -37,6 +47,38 @@ const userSchema = new Schema<IUser>(
     {timestamps: true}
 );
 
+userSchema.statics.changePassword = async function (email: string, password: string, newPassword: string) {
+    const user = await this.findOne({email}) as IUserDocument;
+    if (!user) {
+        throw new Error("User does not exist");
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+        throw new Error("Incorrect password");
+    }
+
+    user.password = newPassword;
+    await user.save();
+}
+
+userSchema.statics.forgetPassword = async function (email: string, candidatePassword: string): Promise<void> {
+    const user = await this.findOne({email}) as IUserDocument;
+    if (!user) {
+        throw new Error("User does not exist");
+    }
+
+    if (!candidatePassword) {
+        throw new Error("candidatePassword does not exist");
+    }
+
+    user.password = candidatePassword;
+    await user.save();
+}
+
+userSchema.methods.comparePassword = async function (candidatePassword: string) {
+    return await bcrypt.compare(candidatePassword, this.password);
+}
+
 userSchema.pre("save", async function (next) {
     if (this.isModified("password")) {
         this.password = await bcrypt.hash(this.password, 10);
@@ -44,6 +86,6 @@ userSchema.pre("save", async function (next) {
     next();
 });
 
-const User = models.User || model<IUser>("User", userSchema);
+const User = (models.User as IUserSchema || model<IUserDocument, IUserSchema>("User", userSchema));
 
 export default User;
